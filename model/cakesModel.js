bce64960516ee85a5fb1bbd6a40b7aed829162c0
ameentm24 <toameentm@gmail.com>
2025-01-24 17:34:07 +0530

    cake update

--- model/cakesModel.js
+++ model/cakesModel.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const cakesSchema = new mongoose.Schema({
+  // category: { type: String, required: true },
+  // items: [
+  //   {
+  //     itemImage: { type: String, required: true },
+  //     title: { type: String, required: true },
+  //     description: { type: String, required: true },
+  //     price: { type: String, required: true },
+  //     isVeg: { type: Boolean, required: true }, // Added for vegetarian status
+  //     isDeliverable: { type: Boolean, required: true }, // Added for deliverable status
+  //   },
+  // ],
+
+  itemName: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  itemImages: { type: [String], required: true, default: [] },
   category: { type: String, required: true },
-  items: [
-    {
-      img: { type: String, required: true },
-      title: { type: String, required: true },
-      description: { type: String, required: true },
-      price: { type: String, required: true },
-      isVeg: { type: Boolean, required: true }, // Added for vegetarian status
-      isDeliverable: { type: Boolean, required: true }, // Added for deliverable status
-    },
-  ],
+  price: { type: Number, required: true },
+  isVeg: { type: Boolean, required: true, default: true },
+  isDeliverable: { type: Boolean, required: true, default: true },
+},{
+  timestamps: true
 });
 
 module.exports = mongoose.model("Cakes", cakesSchema);
